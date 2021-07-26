737932cb553a1c71746b9b10fc6d1eaf36157c9b
김지환 <bure5kzam@gmail.com>
2021-07-27 07:26:01 +0900

    chore: express.json, express.urlencoded 추가

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,8 +3,19 @@ const routes = require("./routes");
 require("dotenv").config();
 const { PORT } = process.env;
 const app = express();
+
+//cors
+const cors = require("cors");
+app.use(cors);
+
 //middleware
 app.use("/", routes);
+
+// content-type - application/json
+app.use(express.json());
+
+// content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT || 8080, () => {
   console.log(PORT || 8080);
