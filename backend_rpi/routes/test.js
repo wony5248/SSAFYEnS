1f07e0781bba69073cccdd792fde98f8f72d6fd5
wony5248 <wony5248@gmail.com>
2021-07-29 00:55:52 +0900

    api modify

--- backend_rpi/routes/test.js
+++ backend_rpi/routes/test.js
@@ -2,24 +2,32 @@ var express = require("express");
 var router = express.Router();
 const axios = require("axios");
 var fs = require("fs");
-
+var sensorData = [];
 /*DB 에서 가져올 센서 값*/
 router.get("/sensor", function (req, res, next) {
   //   res.send({ "temp": "36", "humid": "56", "noise": "168", "light": "32" });
-  res.json(req.body);
+  res.json({"temp" : `${sensorData[0]}`, "humid" : `${sensorData[1]}`, "noise" : `${sensorData[2]}`, "light" : `${sensorData[3]}`});
+
+  
 });
 
 router.post("/sensor", function (req, res, next) {
-//   res.send("DB로 보낼 센서값");
-  res.json(req.body);
+  //   res.send("DB로 보낼 센서값");
+  sensorData = []
+  sensorData.push(req.body["temp"]);
+  sensorData.push(req.body["humid"]);
+  sensorData.push(req.body["noise"]);
+  sensorData.push(req.body["light"]);
+  console.log(sensorData);
+  res.json(sensorData);
 });
 
 router.post("/timer", function (req, res, next) {
-//   res.send("2");
+  //   res.send("2");
   res.json(req.body);
 });
 router.post("/stopwatch", function (req, res, next) {
-//   res.send("3");
+  //   res.send("3");
   res.json(req.body);
 });
 // router.post('/getModule', function(req, res, next) {
