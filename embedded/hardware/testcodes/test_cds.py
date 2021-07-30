ab55942207e39a70081614e63359355f97939a83
eunjee <ssej0221@gmail.com>
2021-07-30 11:05:54 +0900

    test/testcode/sensors

---
+++ embedded/hardware/testcodes/test_cds.py
@@ -0,0 +1,18 @@
+# adc: MCP3008
+import spidev, time
+
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.max_speed_hz = 1350000
+
+def analog_read(channel):
+    r = spi.xfer2([1, (8 + channel) << 4, 0])
+    adc_out = ((r[1]&3) << 8) + r[2]
+
+    return adc_out
+
+while True:
+    reading = analog_read(0)
+    voltage = reading * 3.3 / 1024
+    print("Reading=%d\tVoltage=%f" % (reading, voltage))
+    time.sleep(1)
