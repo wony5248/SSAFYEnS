2628b5f1da3640605e190ebb46e53576729f4b9a
김지환 <bure5kzam@gmail.com>
2021-08-06 12:43:13 +0900

    fix: user 모델에 user_id 외래키 버그 수정

--- backend/src/models/users.js
+++ backend/src/models/users.js
@@ -2,7 +2,7 @@
 const { Model } = require("sequelize");
 const { user_id } = require("../validation/averageValidation");
 module.exports = (sequelize, DataTypes) => {
-  class schedules extends Model {
+  class users extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -10,30 +10,28 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.schedules.hasMany(models.users, { foreignKey: "user_id" });
+      models.users.belongsTo(models.users, { foreignKey: user_id });
     }
   }
-  schedules.init(
+  users.init(
     {
-      user_id: DataTypes.STRING,
-      month: DataTypes.STRING,
-      year: DataTypes.STRING,
-      week: DataTypes.STRING,
-      date: DataTypes.DATE,
-      title: DataTypes.STRING,
-      context: DataTypes.STRING,
-      started_at: DataTypes.DATE,
-      finished_at: DataTypes.DATE,
-      deadline_at: DataTypes.DATE,
-      point: DataTypes.INTEGER,
-      is_finished: DataTypes.BOOLEAN,
-      notification: DataTypes.DATE,
-      noti_extend: DataTypes.INTEGER,
-      challenge_id: DataTypes.INTEGER,
+      user_id: {
+        type: DataTypes.STRING,
+        primaryKey: true, // Needed for Sequelize id, https://sequelize.org/master/manual/legacy.html
+      },
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      cellphone: DataTypes.STRING,
+      password: DataTypes.STRING,
+      exp: DataTypes.INTEGER,
+      is_admin: DataTypes.BOOLEAN,
     },
     {
       sequelize,
-      modelName: "schedules",
+      modelName: "users",
+      createdAt: "created_at",
+      updatedAt: false,
     }
   );
-  return schedules;
+  return users;
+};
