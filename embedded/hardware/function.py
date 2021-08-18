67f98363f3b8013d8fb28af918d8f627e2735cc3
eunjee <ssej0221@gmail.com>
2021-08-19 05:42:41 +0900

    feat: Add null data error handling

--- embedded/hardware/function.py
+++ embedded/hardware/function.py
@@ -111,20 +111,28 @@ def add_schedule(seq=0):
 def brief_schedule(seq=0):
     print("briefing")
     data = server.get(WHOLE_SCHEDULE, date=True)
-    if len(data) == 0:
-        os.system(f'aplay {CUR_DIR}/tts_wav/no_schedule.wav')
-    else:
-        brief_cur_schedule()
-        synthesize_ssml(make_day_briefing(data))
+    try:
+        if len(data) == 0:
+            os.system(f'aplay {CUR_DIR}/tts_wav/no_schedule.wav')
+        else:
+            brief_cur_schedule()
+            synthesize_ssml(make_day_briefing(data))
+    except Exception as e:
+        print("Server error: ", e)
+        os.system(f'aplay {CUR_DIR}/tts_wav/problem_occurred.wav')
     return False
 
 def brief_cur_schedule(seq=0):
     print("cur schedule")
     data = server.get(CUR_SCHEDULE, date=True)
-    if len(data) == 0:
-        os.system(f'aplay {CUR_DIR}/tts_wav/no_cur_schedule.wav')
-    else:
-        synthesize_ssml(make_cur_schedule(data))
+    try:
+        if len(data) == 0:
+            os.system(f'aplay {CUR_DIR}/tts_wav/no_cur_schedule.wav')
+        else:
+            synthesize_ssml(make_cur_schedule(data))
+    except Exception as e:
+        print("Server error: ", e)
+        os.system(f'aplay {CUR_DIR}/tts_wav/problem_occurred.wav')
     return False
 
 def edit_schedule(seq=0):
@@ -178,23 +186,27 @@ def clear_schedule(seq=0):
     # 현재 진행중인 일정 가져오기
     # 있으면 완료, 없으면 없다고 알리기
     data = server.get(CUR_SCHEDULE, date=True)
-    if len(data) == 0:
-        os.system(f'aplay {CUR_DIR}/tts_wav/no_cur_schedule.wav')
-    else:
-        # 완료 api 요청
-        changed_value = {
-            "started_at": data['started_at'],
-            "finished_at": server.get_timeline(True),
-            "deadline_at": data['deadline_at'],
-            "is_finished": True
-        }
-        # 일단 임의로 첫번째 진행중인 일정 삭제
-        success = server.put(changed_value, SPECIFIC_SCHEDULE, data[0]['schedule_id'])
-        reset_data()
-        if success:
-            os.system(f'aplay {CUR_DIR}/tts_wav/clear_schedule_finish.wav')
+    try:
+        if len(data) == 0:
+            os.system(f'aplay {CUR_DIR}/tts_wav/no_cur_schedule.wav')
         else:
-            os.system(f'aplay {CUR_DIR}/tts_wav/problem_occurred.wav')
+            # 완료 api 요청
+            changed_value = {
+                "started_at": data['started_at'],
+                "finished_at": server.get_timeline(True),
+                "deadline_at": data['deadline_at'],
+                "is_finished": True
+            }
+            # 일단 임의로 첫번째 진행중인 일정 삭제
+            success = server.put(changed_value, SPECIFIC_SCHEDULE, data[0]['schedule_id'])
+            reset_data()
+            if success:
+                os.system(f'aplay {CUR_DIR}/tts_wav/clear_schedule_finish.wav')
+            else:
+                os.system(f'aplay {CUR_DIR}/tts_wav/problem_occurred.wav')
+    except Exception as e:
+        print("Server error: ", e)
+        os.system(f'aplay {CUR_DIR}/tts_wav/problem_occurred.wav')
     return False
 
 
