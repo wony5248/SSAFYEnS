b564ea0b395dcdc2b1a7a303def2b9a3fd846ea7
김지환 <bure5kzam@gmail.com>
2021-08-19 06:32:15 +0000

    ci/cd: apt.sh 추가

--- sh/backend_build.sh
+++ sh/backend_build.sh
@@ -8,21 +8,13 @@ SHELL_PATH=$(dirname $(realpath $0))
 echo docker pull mariadb/server $MARIADB_VERSION 
 sudo docker pull mariadb/server:$MARIADB_VERSION &&
 
+echo remove mariadb
+sudo docker stop sfens-mariadb
+sudo docker rm sfens-mariadb
+sudo docker 
 
 echo run mariaDB image... &&
 docker run -p $MARIADB_PORT:3306  --name sfens-mariadb -e MARIADB_ROOT_PASSWORD=rlathdgus -d mariadb/server:$MARIADB_VERSION &&
   
-echo cd ...&&
-cd S05P13A109/frontend/&&
 
-echo build delete...&&
-rm -rf  ./build&&
 
-echo build...&&
-npm run build &&
-
-echo build... &&
-docker build -t sfens-nginx . &&
-
-echo run... &&
-docker run -d --name sfens-nginx -p $HTTP_PORT:80 sfens-nginx 
--- sh/frontend_build.sh
+++ sh/frontend_build.sh
@@ -3,25 +3,25 @@ NGINX_PORT=80
 
 SHALL_PATH=$(dirname $(realpath $0))
 
-echo cd 
-cd "$SHALL_PATH/../frontend"
+echo cd &&
+cd "$SHALL_PATH/../frontend"&&
 
-echo git: pull origin from develop 
-git pull origin develop
+echo git: pull origin from develop &&
+git pull origin develop &&
 
-echo react: remove exist build file at $SHALL_PATH
-rm -rf  ./build
+echo react: remove exist build file at $SHALL_PATH &&
+rm -rf  ./build &&
 
-echo nginx: remove exist container, image 
-sudo docker stop sfens-nginx &&
-sudo docker rm sfens-nginx &&
-sudo docker rmi sfens-nginx &&
+echo nginx: build file &&
+npm run build &&
 
-echo nginx: build
-docker build -t sfens-nginx .
+echo nginx: remove exist container, image &&
+sudo docker stop sfens-nginx 
+sudo docker rm sfens-nginx 
+sudo docker rmi sfens-nginx 
 
-echo nginx: build file
-npm run build
+echo nginx: build &&
+docker build -t sfens-nginx . &&
 
-echo nginx: run
-docker run -d --name sfens-nginx -p $NGINX_PORT:80 sfens-nginx
+echo nginx: run &&
+docker run -d --name sfens-nginx -p $NGINX_PORT:80 sfens-nginx 
