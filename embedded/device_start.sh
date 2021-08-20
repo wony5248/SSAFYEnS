727a13f2e8905a8e2e302781431b3754323afe62
eunjee <ssej0221@gmail.com>
2021-08-20 11:32:26 +0900

    feat: add streaming booting script

--- embedded/device_start.sh
+++ embedded/device_start.sh
@@ -5,6 +5,7 @@ echo $SHELL_PATH
 BACKEND_PATH='backend_rpi'
 FRONTEND_PATH='frontend_rpi'
 HARDWARE_PATH='hardware'
+STREAMING_PATH='streaming'
 # Need to be fixed when change the key
 GCP_KEY='ssafyens-71ea8d7e740e.json'
 
@@ -19,15 +20,19 @@ start_frontend(){
 	# electron should be started in monitor terminal
 }
 
-start_hardware(){
+start_main_event(){
 	cd $SHELL_PATH/$HARDWARE_PATH
 	export GOOGLE_APPLICATION_CREDENTIALS="./$GCP_KEY"
 	python3 event_listener.py
 	# python3 event_listener.py --mode call 
-	# TODO: Add Streaming code
+}
+start_streaming(){
+	cd $SHELL_PATH/$HARDWARE_PATH/$STREAMING_PATH
+	python3 camera_stream.py
 }
 
 start_backend > $SHELL_PATH/server_log.log &
 start_frontend > $SHELL_PATH/front_log.log &
-start_hardware > $SHELL_PATH/hardware_log.log &
+start_main_event > $SHELL_PATH/hardware_log.log &
+start_streaming
 echo "start everything" 
